94751b37a472f1b0631fde4c1b0f1a4038a7fe2d
LiderBiomedico <coordinadorbiomedica@hosusana.gov.co>
2026-05-04 17:39:38 -0500

    Delete netlify/functions directory

--- netlify/functions/health.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function() {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ status: 'ok', version: '3.3', platform: 'netlify' })
-  };
-};
